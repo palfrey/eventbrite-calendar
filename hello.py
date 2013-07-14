28d6aecc2cf4cb8f5571734e62e2da17d587992a
Tom Parker <palfrey@tevp.net>
2013-07-14 13:51:50 +0100

    Add initial Bootstrap work

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
+
+app.config['BOOTSTRAP_USE_MINIFIED'] = True
+app.config['BOOTSTRAP_USE_CDN'] = True
+app.config['BOOTSTRAP_FONTAWESOME'] = True
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+	return render_template('index.html')
+
+if '__main__' == __name__:
+	app.run(debug=True)
